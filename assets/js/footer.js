927de1f93eadf77940eaae010c8ec6068173810a
Charlie <charlie@makedo.net>
2019-02-25 17:54:34 +0000

    add track-focus

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,33 +2,36 @@
 window.jQuery = window.$ = jQuery;
 
 // Modules.
-import { deBounce, isHighDensity, isRetina, compareRetina, compareBreakpoint } from './modules/utils.js';
+
+import {
+	deBounce,
+	isHighDensity,
+	isRetina,
+	compareRetina,
+	compareBreakpoint,
+} from './modules/utils.js';
 import skipLink from './modules/skip-link.js';
+import trackFocus from './modules/track-focus.js';
 import styleGuide from './modules/style-guide.js';
 import initResponsiveBackgroundImages from './modules/rwd-bg-images.js';
 
 // Ready.
-window.addEventListener(
-	'DOMContentLoaded',
-	function() {
-		skipLink();
-		styleGuide();
-		initResponsiveBackgroundImages($);
-	}
-);
+window.addEventListener('DOMContentLoaded', function() {
+	trackFocus(document.body);
+	skipLink();
+	styleGuide();
+	initResponsiveBackgroundImages($);
+});
 
 // Load.
-window.addEventListener(
-	'load',
-	function() {
-		responsiveBackgroundImages('.js-bg-img');
-	}
-);
+window.addEventListener('load', function() {
+	responsiveBackgroundImages('.js-bg-img');
+});
 
 // Resize
 window.addEventListener(
 	'resize',
 	deBounce(() => {
 		responsiveBackgroundImages('.js-bg-img');
-	}, 100)
+	}, 100),
 );
