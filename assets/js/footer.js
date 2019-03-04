14e9f19843dc6c042b3ad5e0977b83a3d07166a3
Charlie <charlie@makedo.net>
2019-03-04 16:04:38 +0000

    add track-focus

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,7 +2,6 @@
 window.jQuery = window.$ = jQuery;
 
 // Modules.
-
 import {
 	deBounce,
 	isHighDensity,
@@ -10,23 +9,22 @@ import {
 	compareRetina,
 	compareBreakpoint,
 } from './modules/utils.js';
+import trackFocus from './modules/track-focus.js';
 import skipLink from './modules/skip-link.js';
 import trackFocus from './modules/track-focus.js';
 import styleGuide from './modules/style-guide.js';
 import initResponsiveBackgroundImages from './modules/rwd-bg-images.js';
 
 // Ready.
-window.addEventListener(
-	'DOMContentLoaded',
-	function() {
-		// Replace no-js with js on the root HTML element.
-		document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/g, '') + ' js ';
-		trackFocus(document.body);
-		skipLink();
-		styleGuide();
-		initResponsiveBackgroundImages($);
-	}
-);
+window.addEventListener('DOMContentLoaded', function() {
+	// Replace no-js with js on the root HTML element.
+	document.documentElement.className =
+		document.documentElement.className.replace(/\bno-js\b/g, '') + ' js ';
+	trackFocus(document.body);
+	skipLink();
+	styleGuide();
+	initResponsiveBackgroundImages($);
+});
 
 // Load.
 window.addEventListener('load', function() {
