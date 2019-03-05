5159ea4e2b91a827628a464ebd6280bafd0624c0
Charlie <charlie@makedo.net>
2019-03-05 11:40:31 +0000

    stylelint rule ignore fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	extends: ['stylelint-config-wordpress/scss', 'stylelint-config-prettier'],
 	plugins: ['stylelint-scss'],
 	rules: {
-		// 'block-no-empty': null,
+		'block-no-empty': null,
 		'no-descending-specificity': null,
 		'declaration-property-unit-whitelist': null,
 	},
