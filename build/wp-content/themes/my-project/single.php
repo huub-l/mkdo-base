bb2d04c32a7074aaeb1ac3d36346736a3e0961b1
Charlie <charlie@makedo.net>
2019-03-05 14:08:47 +0000

    minor fix to single.php

--- build/wp-content/themes/my-project/single.php
+++ build/wp-content/themes/my-project/single.php
@@ -18,7 +18,7 @@ get_header();
 	while ( have_posts() ) {
 		the_post();
 
-		get_template_part( 'template-parts/modules/content', 'single' );
+		get_template_part( 'template-parts/content', 'single' );
 
 		the_post_navigation();
 
