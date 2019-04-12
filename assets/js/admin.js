6e50cf345e5c30775881ca067b92ae6096e99b69
gemplank <info@gpwebdevelopment.co.uk>
2019-04-12 16:29:16 +0100

    Add missing svg upload code

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,10 +3,22 @@ window.jQuery = window.$ = jQuery;
 
 // Modules.
 import { deBounce } from './modules/utils.js';
+import { mediaGridObserver, attachmentPreviewObserver } from './modules/media-library.js';
 
 // Ready.
 window.addEventListener('DOMContentLoaded', function() {
 	// Do something.
+
+	// Courtesty of https://gist.github.com/benfrain/9422862
+	mediaGridObserver.observe(document.body, {
+		childList: true,
+		subtree: true
+	});
+
+	attachmentPreviewObserver.observe(document.body, {
+		childList: true,
+		subtree: true
+	});
 });
 
 // Load.
