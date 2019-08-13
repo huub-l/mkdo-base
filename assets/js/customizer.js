27f764731c8e9213f745dbe89c117ae5eadb0049
Dave Green <hello@davetgreen.me>
2019-08-13 12:04:31 +0100

    Update customizer.js

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -1,5 +1,5 @@
-// Map jQuery to $.
-window.jQuery = window.$ = jQuery;
+// Hello? What's all this jQuery? We'll have no libraries here!
+// Are you vanilla? This is a vanilla file, for vanilla people! There's nothing for jQuery folk here!
 
 // Modules.
 import { deBounce } from './modules/utils.js';
