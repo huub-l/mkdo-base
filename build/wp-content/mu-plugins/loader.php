9f3a99938b1be1cd3afedb197aaa970f00707b2c
Dave Green <dave@makedo.net>
2020-04-24 11:17:53 +0100

    Update loader.php

--- build/wp-content/mu-plugins/loader.php
+++ build/wp-content/mu-plugins/loader.php
@@ -13,3 +13,6 @@ require_once plugin_dir_path( __FILE__ ) . 'kirki/kirki.php';
 require_once plugin_dir_path( __FILE__ ) . 'mkdo-core/plugin.php';
 
 require_once plugin_dir_path( __FILE__ ) . 'mkdo-permissions/plugin.php';
+
+require_once plugin_dir_path( __FILE__ ) . 'mkdo-limit-login-attempts.php';
+
